6bdd963d2d3c8976f4218868feb2fc9ca472ffd6
nvdung1607 <nvdung1607@gmail.com>
2026-08-31 15:42:20 +0700

    chore: bump version to 1.0.4 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nvdung1607.countdayleave"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
